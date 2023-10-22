b38f4a8e084a83c6d83b781aa5a2a820062e79fd
Mugundh <mugundhjb@gmail.com>
2023-10-22 23:55:42 +0530

    Added Search Bar

--- pages/index.js
+++ pages/index.js
@@ -56,6 +56,7 @@ import Product from "../components/Product";
 
 export default function Home(){
   const [productsInfo, setProductsInfo] = useState([]);
+  const [phrase, setPhrase] = useState([]);
   useEffect(() => {
       fetch('/api/products')
       .then((res) => res.json())
@@ -69,26 +70,37 @@ export default function Home(){
   const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
   console.log({categoriesNames}) 
 
-  
+  let products;
+  // search bar
+    if(phrase) {
+      products = productsInfo.filter(p => p.name.toLowerCase().includes(phrase))
+    }
+    else{
+      products = productsInfo
+    }
+
   return(
     <div className="p-5">
+    <input type="text" value={phrase} onChange={e => setPhrase(e.target.value)} placeholder="Search for products..." className="bg-gray-100 w-full py-2 px-4 rounded-xl"></input>
+    
       <div>
       {categoriesNames.map(categoryName => (
         <div key={categoryName}>
+        {products.find(p => p.category === categoryName) && (
+          <div>
           <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
           
             <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
-            {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
+            {products.filter(p => p.category === categoryName).map(productInfo => (
               <div key={productInfo._id} className="px-5 snap-start">
               <Product {...productInfo} />
               </div>            
             ))}
             </div>
+          </div>
+        )}  
         </div>
-      ))}
-        <div className="py-4">
-          
-        </div> 
+      ))} 
       </div>
     </div>
   )

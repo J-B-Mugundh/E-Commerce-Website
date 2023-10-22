06a87e28dcae281ab10ae825c40d212054890455
Mugundh <mugundhjb@gmail.com>
2023-10-22 23:41:29 +0530

    Added Horizontal Scroll Bar

--- pages/index.js
+++ pages/index.js
@@ -75,11 +75,11 @@ export default function Home(){
       <div>
       {categoriesNames.map(categoryName => (
         <div key={categoryName}>
-          <h2 className="text-2xl capitalize">{categoryName}</h2>
+          <h2 className="text-2xl py-5 capitalize">{categoryName}</h2>
           
-            <div className="flex">
+            <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
             {productsInfo.filter(p => p.category === categoryName).map(productInfo => (
-              <div key={productInfo._id} className="px-5">
+              <div key={productInfo._id} className="px-5 snap-start">
               <Product {...productInfo} />
               </div>            
             ))}

84ba96987b0457c3d8e654511a5117b9573603e5
Mugundh <mugundhjb@gmail.com>
2023-10-29 20:07:26 +0530

    Saving orders to DB

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -52,6 +52,7 @@
 // }
 
 import {initMongoose} from '../../lib/mongoose'
+import Order from '../../models/Order';
 import Product from '../../models/Product'
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res){
@@ -83,14 +84,27 @@ export default async function handler(req, res){
 
   // res.json(products);
   // return;
+
+  const order = await Order.create({
+    products: line_items,
+    name, 
+    email,
+    address,
+    city,
+    paid: 0
+  });
+
   const session = await stripe.checkout.sessions.create({
     line_items: line_items,
-    mode: 'payment',
+    mode: 'payment', 
     customer_email: email,
     success_url: `${req.headers.origin}/?success=true`,
     cancel_url: `${req.headers.origin}/?canceled=true`,
+    metadata: {orderId: order._id.toString()}
   });
   res.redirect(303, session.url);
+
   
-  res.json(req.method);
+
+  //res.json(req.method);
 }

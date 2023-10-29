7380270cf3575f31388856c42a47bfebf9fc746b
Mugundh <mugundhjb@gmail.com>
2023-10-29 22:09:36 +0530

    Updated comments

--- components/Layout.js
+++ components/Layout.js
@@ -1,40 +1,26 @@
-// import Footer from "./Footer";
-// import {useContext, useEffect, useState} from "react";
-// import {ProductsContext} from "./ProductsContext";
-
-// export default function Layout({children}) {
-//   const {setSelectedProducts} = useContext(ProductsContext);
-//   const [success,setSuccess] = useState(false);
-//   useEffect(() => {
-//     if (window.location.href.includes('success')) {
-//       setSelectedProducts([]);
-//       setSuccess(true);
-//     }
-//   }, []);
-//   return (
-//     <div>
-//       <div className="p-5">
-//         {success && (
-//           <div className="mb-5 bg-green-400 text-white text-lg p-5 rounded-xl">
-//             Thanks for your order!
-//           </div>
-//         )}
-//         {children}
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
 
 import Footer from "./Footer";
 import {useContext, useEffect, useState} from "react";
 import {ProductsContext} from "./ProductsContext";
 
 export default function Layout({ children }) {
+  const {setSelectedProducts} = useContext(ProductsContext);
+  const [success, setSuccess] = useState(false);
+  useEffect(() => {
+    if(window.location.href.includes('success')){
+      setSelectedProducts([]);
+      setSuccess(true);
+    }
+  }, [])
   // making a layout so that the body and footer can be one
   return (
     <div>
       <div className="p-5">  {/* main div */}
+      {success && (
+        <div className="mb-5 bg-green-400 text-white text-lg p-5 rounded-xl">
+          Thanks for your order!
+        </div>
+      )}
         {children}
       </div>
       <Footer /> {/* Footer */}
